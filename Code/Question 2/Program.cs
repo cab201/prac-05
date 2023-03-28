ec272322b5ec50ad092a554d9b0bed0474db05cb
unknown <MeltUser01@qut.edu.au>
2023-03-28 10:49:14 +1000

    Added question 3

---
+++ Code/Question 2/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Question_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = GetDirectoryName();
+            CheckDirectory(path);
+        }
+
+        // Get the path to the directory from the user
+        static string GetDirectoryName()
+        {
+            string currentDir = Environment.CurrentDirectory;
+            Console.WriteLine("Current directory {0}", currentDir);
+            Console.WriteLine("Enter path to file:");
+            return Console.ReadLine();
+        }
+
+        // Check the directory, its creation, written and accessed
+        // time.
+        // Also show sub directories and files
+        static void CheckDirectory(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                Console.WriteLine("Creation time:");
+                Console.WriteLine(Directory.GetCreationTime(path));
+                Console.WriteLine("Written time:");
+                Console.WriteLine(Directory.GetLastWriteTime(path));
+                Console.WriteLine("Accessed time:");
+                Console.WriteLine(Directory.GetLastAccessTime(path));
+                // Get the files in the directory
+                string[] files = Directory.GetFiles(path);
+                // Get the sub-folders in the directory
+                string[] subDirs = Directory.GetDirectories(path);
+                Console.WriteLine("This directory has {0} files and {1} sub-folders.",
+                    files.Length, subDirs.Length);
+            } else
+            {
+                Console.WriteLine("Directory at path {0} does not exist", path);
+            }
+        }
+    }
+}
---
+++ Code/Question 3/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Question_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = GetFileName();
+            CheckFile(path);
+        }
+
+        // Get the file name from the user
+        static string GetFileName()
+        {
+            string currentDir = Environment.CurrentDirectory;
+            Console.WriteLine("Current directory {0}", currentDir);
+            Console.WriteLine("What is the path to the file?");
+            return Console.ReadLine();
+        }
+
+        static void CheckFile(string path)
+        {
+            bool fileExists = File.Exists(path);
+            if (fileExists)
+            {
+                // Using StreamReader to read the whole content of a file
+                DisplayFileContent(path);
+                // Using StreamWriter to write to a file
+                using (StreamWriter writer = new StreamWriter(path, append: true))
+                {
+                    // This will add a new line
+                    writer.WriteLine();
+                    string input;
+                    do
+                    {
+                        Console.WriteLine("What do you want to add to the file");
+                        input = Console.ReadLine();
+                        writer.WriteLine(input);
+                    } while (input != "QUIT");
+                }
+            }
+            else
+            {
+                Console.WriteLine("The file does not exist");
+            }
+        }
+
+        static void DisplayFileContent(string path)
+        {
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line = null;
+                // Read through the whole file
+                do
+                {
+                    line = reader.ReadLine();
+                    Console.WriteLine(line);
+                } while (!reader.EndOfStream);
+            }
+        }
+    }
+}
