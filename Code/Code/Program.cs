8586e7ca87fc708d8f0373f1414b595745d84a28
unknown <MeltUser01@qut.edu.au>
2023-03-29 09:27:16 +1000

    Added question 1

---
+++ Code/Code/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = GetPath();
+            CheckFile(path);
+            Console.ReadLine();
+        }
+
+        static string GetPath()
+        {
+            string currentDir = Environment.CurrentDirectory;
+            Console.WriteLine("Current directory: {0}", 
+                currentDir);
+            Console.WriteLine
+                ("What is the path to the file?");
+            return Console.ReadLine();
+        }
+
+        static void CheckFile(string path)
+        {
+            bool fileExist = File.Exists(path);
+
+            if (fileExist)
+            {
+                DateTime createTime = 
+                    File.GetCreationTime(path);
+                DateTime accessTime = 
+                    File.GetLastAccessTime(path);
+                DateTime writeTime = 
+                    File.GetLastWriteTime(path);
+                Console.WriteLine
+                    ("Create time: {0}", createTime);
+                Console.WriteLine
+                    ("Access time: {0}", accessTime);
+                Console.WriteLine
+                    ("Write time: {0}", writeTime);
+            } else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine
+                    ("Error: No file exists at path {0}",
+                    path);
+            }
+        }
+    }
+}
