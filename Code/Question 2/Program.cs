b83e114f38c746860346a486d40a79a40a5f94ce
unknown <MeltUser01@qut.edu.au>
2023-03-29 10:26:23 +1000

    added question 3 and 4

---
+++ Code/Question 2/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Question_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = GetPath();
+            CheckFile(path);
+            Console.ReadLine();
+        }
+
+        static string GetPath()
+        {
+            string currentDir = Environment.CurrentDirectory;
+            Console.WriteLine("Current directory: {0}",
+                currentDir);
+            Console.WriteLine
+                ("What is the path to the directory?");
+            return Console.ReadLine();
+        }
+
+        static void CheckFile(string path)
+        {
+            bool dirExists = Directory.Exists(path);
+
+            if (dirExists)
+            {
+                DateTime createTime =
+                    Directory.GetCreationTime(path);
+                DateTime accessTime =
+                    Directory.GetLastAccessTime(path);
+                DateTime writeTime =
+                    Directory.GetLastWriteTime(path);
+                Console.WriteLine
+                    ("Create time: {0}", createTime);
+                Console.WriteLine
+                    ("Access time: {0}", accessTime);
+                Console.WriteLine
+                    ("Write time: {0}", writeTime);
+
+                string[] subDirs = Directory.GetDirectories(path);
+                string[] files = Directory.GetFiles(path);
+                Console.WriteLine("There are {0} sub directories"
+                    + " and {1} files", subDirs.Length, files.Length);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine
+                    ("Error: No directory exists at path {0}",
+                    path);
+            }
+        }
+    }
+}
---
+++ Code/Question 3 and 4/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace Question_3_and_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = GetPath();
+            ReadFile(path);
+
+            // Print to a file
+            //Console.WriteLine("Printing to a file...");
+            path = GetPath();
+            WriteFile(path);
+        }
+
+        static string GetPath()
+        {
+            Console.WriteLine(Environment.CurrentDirectory);
+            Console.WriteLine("What is the path to the file?");
+            return Console.ReadLine();
+        }
+
+        static void ReadFile(string path)
+        {
+            if (File.Exists(path))
+            {
+                ReadFileContent(path);
+            } else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error: No file exists at {0}",
+                    path);
+            }
+        }
+
+        static void ReadFileContent(string path)
+        {
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line = reader.ReadLine();
+                Console.WriteLine(line);
+                // Repeat until there's nothing to read
+                while (line != null)
+                {
+                    line = reader.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+        }
+
+        // Check if a file exist. If it does, write something to it.
+        static void WriteFile(string path)
+        {
+            if (File.Exists(path))
+            {
+                using (StreamWriter writer = new StreamWriter(path))
+                {
+                    writer.WriteLine(@"In fairy-tales, witches always wear silly black hats and black coats, 
+and they ride on broomsticks. 
+But this is not a fairy-tale. 
+This is about REAL WITCHES.");
+                }
+            } else
+            {
+                Console.WriteLine("Error: No file exists at path {0}", path);
+            }
+        }
+    }
+}
